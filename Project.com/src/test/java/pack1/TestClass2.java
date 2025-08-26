986b78404dd10d1bd1769ad7df7d8495f9b1215b
Ro-hit10 <rohitmore8483@gmail.com>
2025-08-26 20:19:12 +0530

    Update TestClass2.java
    Commit $ from GitHub - dded # methods to TestClass2

--- Project.com/src/test/java/pack1/TestClass2.java
+++ Project.com/src/test/java/pack1/TestClass2.java
@@ -4,7 +4,10 @@ public class TestClass2 {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Method from TestClass2");
+		System.out.println("Method 1 from TestClass2");
+		System.out.println("Method 2 from TestClass2");
+		System.out.println("Method 3 from TestClass2");
 	}
 
 }
+
