ffacb97617dede04f789f1a148ae6709c8398d38
Rohit More <rohitmore8483>
2025-08-26 20:16:17 +0530

    Third Commit - TestClass-2 Created

--- Project.com/src/test/java/pack1/TestClass1.java
+++ Project.com/src/test/java/pack1/TestClass1.java
@@ -2,4 +2,10 @@ package pack1;
 
 public class TestClass1 {
 
+	public static void main(String[] args) {
+		System.out.println(" Mehtod From TestClass1 ");
+		
+	}
+	
+
 }
---
+++ Project.com/src/test/java/pack1/TestClass2.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class TestClass2 {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Method from TestClass2");
+	}
+
+}
